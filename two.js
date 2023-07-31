4d87db075e222ab2f2f2e6122f13343175c5716f
shaliga-saravanan <140493054+shaliga-saravanan@users.noreply.github.com>
2023-07-31 16:31:44 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,6 @@
+function on(){
+    document.getElementById('img').src="https://cdn.pixabay.com/photo/2017/05/09/10/59/mount-fuji-2297961_1280.jpg"
+}
+function off(){
+    document.getElementById('img').src="https://cms.accuweather.com/wp-content/uploads/2017/05/sunset.jpg"
+}
